ac1086ea448fb03a2ed4c982aa625a413272debc
jm-moore <jmmoore9358@gmail.com>
2022-09-13 11:10:44 -0500

    initial commit

---
+++ OpenWeatherAPI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+
+// API Key = 2f8f253ca1d0b89568a167e2b320e678
+
+var client = new HttpClient();
+
+Console.WriteLine($"Welcome to the Open Weather API!\nPlease enter your API key:");
+var api_key = Console.ReadLine();
+
+Console.WriteLine("Enter the city that you want the current forecast of:");
+var city_name = Console.ReadLine().ToLower();
+
+var URL = $"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=imperial";
+
+var weather = client.GetStringAsync(URL).Result;
+
+var formattedResponseMain = JObject.Parse(weather);
+var formattedResponseTemp = formattedResponseMain["main"]["temp"];
+
+Console.WriteLine($"\n{formattedResponseTemp} degrees fahrenheit.");
